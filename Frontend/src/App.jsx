016324a54cee13964c6ea62fc786bb9db3a533d4
gyanavardhana <gyanavardhanmamidisetti@gmail.com>
2024-07-21 18:16:41 +0530

    setting page completed,mlpage partial

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,12 @@ import Homepage from './Components/Homepage/Home';
 import SignupPage from './Components/Signup/Signup';
 import LoginPage from './Components/Login/Login';
 import PlotComponent from './Components/Plot';
+import SettingsPage from './Components/Settingspage/Settings';
+import MLComponent from './Components/MLpage/MLpage';
+import 'primereact/resources/themes/saga-blue/theme.css'; // or any other theme
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 import './index.css'
 const App = () => {
   return (
@@ -12,7 +18,8 @@ const App = () => {
       <Route path="/signup" element={<SignupPage />}/>
       <Route path="/login" element={<LoginPage />}/>
       <Route path="/plot" element={<PlotComponent />}/>
-      
+      <Route path="/settings/" element={<SettingsPage />} />  
+      <Route path="/mlupload" element={<MLComponent />} />
     </Routes>
     </>
   )
